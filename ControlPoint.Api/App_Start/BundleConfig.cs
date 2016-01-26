18902bf5919361758379e07c6ab19019ee14500d
vinbrown2 <vincent.brown@ryan.com>
2016-01-26 09:33:29 -0500

    add missing files that VS2015 didn't see

--- ControlPoint.Api/App_Start/BundleConfig.cs
+++ ControlPoint.Api/App_Start/BundleConfig.cs
@@ -2,7 +2,7 @@
 using System.Web.Optimization;
 
 namespace ControlPoint.Api
-{
+{ 
 	public class BundleConfig
 	{
 		// For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
